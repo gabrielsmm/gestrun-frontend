50b6aec20512a5b9cfcb32e73455ba904cf33c3c
gabrielsmm <gabrielsmm251@gmail.com>
2025-09-24 21:50:57 -0300

    chore: setup inicial do frontend com Angular Material, TailwindCSS, ngx-toastr, ngx-mask, @auth0/angular-jwt e ngx-ui-loader

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
+import { provideToastr } from 'ngx-toastr';
+import { NgxUiLoaderConfig, NgxUiLoaderModule } from 'ngx-ui-loader';
 
 import { routes } from './app.routes';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  // your configuration here
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimations(),
+    provideToastr(),
+    provideEnvironmentNgxMask(),
+    importProvidersFrom(
+      NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
+    )
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,40 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { NgxMaskDirective } from 'ngx-mask';
+import { ToastrService } from 'ngx-toastr';
+import { NgxUiLoaderModule, NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NgxMaskDirective,
+    NgxUiLoaderModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
+
   protected readonly title = signal('gestrun-frontend');
+
+  constructor(private toastr: ToastrService,
+              private ngxService: NgxUiLoaderService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  showSuccess() {
+    this.ngxService.start();
+    this.ngxService.startBackground("do-background-things");
+
+    setTimeout(() => {
+      this.ngxService.stop();
+      this.ngxService.stopBackground("do-background-things");
+      this.toastr.success('Tarefa concluída com sucesso!', 'Processamento finalizado');
+    }, 5000);
+  }
+
 }
