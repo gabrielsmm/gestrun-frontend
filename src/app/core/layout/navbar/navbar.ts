f6e45e87d002d4a8f506eb8fd9eca64893b41bdc
gabrielsmm <gabrielsmm251@gmail.com>
2025-09-27 12:05:19 -0300

    feat(layout): adicionar navbar com menu de usuário

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from './../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  @Output() toggleSidenav = new EventEmitter<void>();
+
+  deslogar() {
+    this.authService.deslogar();
+  }
 
 }
